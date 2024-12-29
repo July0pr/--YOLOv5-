a11e3a082b0fc04037e4ae8e295e8d1811de212b
July <a847344894@outlook.com>
2024-12-29 19:55:48 +0800

    Add yolov5 as submodule to avoid repo nested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 import json
 from data_puller import DataPuller
 from data_analyzer import DataAnalyzer
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     parser.add_argument('-g', '--repoName', type=str, default='ultralytics/yolov5', help='GitHub 仓库名')
     parser.add_argument('-d', '--dir', type=str, default='./data', help='数据目录')
     args = parser.parse_args()
+
     #请在config.json文件中配置你的token
     with open('./githubToken.json', 'r', encoding='utf-8') as f:
         config_data = json.load(f)
@@ -30,3 +32,5 @@ if __name__ == "__main__":
     analyzer = DataAnalyzer()
     commits, issues, prs = DataPuller.pull_data(config)
     analyzer.analyze_data(config, commits)
+
+    
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import requests
+import os 
+import shutil
 
 def reqTest(proxyPort='7897'):
     '''
@@ -15,3 +17,14 @@ def reqTest(proxyPort='7897'):
         print(f"代理测试失败: {e}")
 
 #reqTest()
+
+def clone_yolov5Repo():
+    os.system(f'git clone https://github.com/ultralytics/yolov5.git')
+
+def rm_yolov5Repo():
+    #删除YOLOv5仓库
+    if os.path.exists('yolov5'):
+        shutil.rmtree('yolov5', onerror=lambda func, path, excinfo: (os.chmod(path, 0o777), func(path)))
+        print(f"YOLOv5仓库已删除")
+    else:
+        print(f"YOLOv5仓库不存在")
